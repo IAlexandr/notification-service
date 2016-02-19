4a4f862b14c07cab2e3950162aebdd4f5995f343
IAlexandr <aivanov@informatica.ru>
2016-02-19 15:33:52 +0300

    wsserver

---
+++ options.js
@@ -0,0 +1,64 @@
+const version = require('./package.json').version;
+const optionsSpec = {
+  SENDER_GMAIL_LOGIN: {
+    required: true,
+    default: 'asdasd',  // без @gmail.com
+    env: 'NS_SENDER_GMAIL_LOGIN'
+  },
+  SENDER_GMAIL_PASS: {
+    required: true,
+    default: '***',
+    env: 'NS_SENDER_GMAIL_PASS'
+  },
+  PORT: {
+    required: true,
+    default: '4445',
+    env: 'NS_PORT'
+  },
+  REG_SOCKET_HOSTS: {
+    require: true,
+    default: "127.0.0.1",
+    env: 'NS_REG_SOCKET_HOSTS',
+    preprocess: function (src) {
+      const sr = src ? src.split(',').map(function (s) {
+        return s.trim();
+      }) : [];
+      return sr;
+    }
+  },
+  NOTIFY_MAIL_LIST: {
+    require: true,
+    default: "asdasd@asdasdasd.ru,asdasd@asdasdasd.ru",
+    env: 'NS_NOTIFY_MAIL_LIST',
+    preprocess: function (src) {
+      const sr = src ? src.split(',').map(function (s) {
+        return s.trim();
+      }) : [];
+      return sr;
+    }
+  },
+};
+
+let options = {
+  version
+};
+
+export default {...options, ...Object.keys(optionsSpec).map((key) => {
+  if (!optionsSpec[key].preprocess) {
+    optionsSpec[key].preprocess = function preprocess (str) {
+      return str;
+    };
+  }
+  const opt = { name: key };
+  if (process.env[optionsSpec[key].env]) {
+    opt.value = optionsSpec[key].preprocess(process.env[optionsSpec[key].env]);
+  } else if (optionsSpec[key].default) {
+    opt.value = optionsSpec[key].preprocess(optionsSpec[key].default);
+  } else if (optionsSpec[key].required) {
+    throw new Error('!!! REQUIRED OPTION NOT SET: ' + key);
+  }
+  return opt;
+}).reduce((prev, cur) => {
+  prev[cur.name] = cur.value;
+  return prev;
+}, {})};
